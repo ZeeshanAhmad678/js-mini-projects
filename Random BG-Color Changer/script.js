c818b76bbfad1030317b9a62d8c39f3e6d088ab7
Zeeshan Ahmad <119109667+ZeeshanAhmad678@users.noreply.github.com>
2025-07-01 08:37:29 +0000

    new small practice uploaded for beginners

---
+++ Random BG-Color Changer/script.js
@@ -0,0 +1,26 @@
+function randomColor(){
+    const hexValues = '0123456789ABCDEF';
+    colorValue = '#';
+    for (let i = 0; i < 6; i++) {
+        colorValue += hexValues[Math.floor(Math.random() * 16)];
+    }
+    return colorValue;
+}
+
+let intervalId = null;
+
+document.querySelector('#start').addEventListener('click', () =>{
+    // if(intervalId == null){
+
+    // }
+    intervalId = setInterval( () => {
+        let currColor = randomColor();
+        document.body.style.backgroundColor = currColor;
+    } , 500)
+});
+
+
+document.querySelector('#stop').addEventListener('click', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+})
